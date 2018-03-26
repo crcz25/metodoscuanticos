2a34fc7f709d65487e247beac752a7175911f980
crcz <A01209474@itesm.mx>
2018-03-26 12:32:58 -0600

    examen con randoms

--- matrixrandom.py
+++ matrixrandom.py
@@ -1,16 +1,16 @@
-import matplotlib.pyplot as plt
-from matplotlib import colors
 import numpy as np
 import random
 
-# n = int(input("Tamaño de matriz n: "))
-# por = int(input("Porcentaje: "))
-n = 5
-por = 60
+n = int(input("Tamaño de matriz n: "))
+por = int(input("Porcentaje: "))
+# n = 20
+# por = 60
 p = int(n * n * (por / 100))
 
 print("Matriz de %d x %d, con porcentaje %d y valor de P=%d" % (n, n, por, p))
+
 mA = np.zeros((n, n), int)
+mB = np.zeros((n, n), int)
 
 unos = 0
 zeros = 0
@@ -20,25 +20,38 @@ helper = int(n * (n-1) / 2)
 upper_triangle = np.triu_indices(n, 1)
 lower_triangle = np.tril_indices(n, -1)
 
-size = helper * 2
-num_zeros = size - p
-zero_one_elements = np.array([0] * num_zeros + [1] * (size-num_zeros))
+# print("Upper triangle", upper_triangle)
+# print("Lower triangle", lower_triangle)
 
-np.random.shuffle(zero_one_elements)
+m = [[], []]
+holi = n - 1
 
-# print(zero_one_elements)
-# print(size)
-# print(num_zeros)
-# print(p)
-# print(np.split(zero_one_elements, 2))
+while True:
+    r1 = random.randint(0, holi)
+    r2 = random.randint(0, holi)
+    r3 = random.randint(0, holi)
 
-aux1, aux2 = np.split(zero_one_elements, 2)
-# print(aux1)
-# print(aux2)
+    # print(m)
+    # print(unos)
 
-mA[upper_triangle] = aux1
-mA[lower_triangle] = aux2
+    if r1 != r2 and r2 != r3 and tuple((r1, r2)) not in m[0] and tuple((r2, r3)) not in m[1]:
+        m[0].append(tuple((r1, r2)))
+        m[1].append(tuple((r2, r3)))
+        # print("non diagonal ", r1, ",", r2)
+        unos += 1
 
+    if unos >= p:
+        break
+
+for i, j in m[0]:
+    mA[i, j] = 1
+
+for i, j in m[1]:
+    mB[i, j] = 1
+
+print("\nMatriz A")
+unos = 0
+zeros = 0
 for i in range(n):
     for j in range(n):
         print(mA[i, j], end=' ')
@@ -48,3 +61,41 @@ for i in range(n):
     print(' | %d' % aux)
 
 print("Total de 0's: %d \nTotal de 1's: %d" % (zeros, unos))
+
+print("\nMatriz B")
+unos = 0
+zeros = 0
+for i in range(n):
+    for j in range(n):
+        print(mB[i, j], end=' ')
+    aux = np.count_nonzero(mB[i] == 1)
+    unos += aux
+    zeros += np.count_nonzero(mB[i] == 0)
+    print(' | %d' % aux)
+
+print("Total de 0's: %d \nTotal de 1's: %d" % (zeros, unos))
+
+# print(np.diag(mA))
+# print(np.diag(mB))
+# print(mA)
+
+
+# size = helper * 2
+# num_zeros = size - p
+# zero_one_elements = np.array([0] * num_zeros + [1] * (size-num_zeros))
+#
+# np.random.shuffle(zero_one_elements)
+#
+# print(zero_one_elements)
+# print(size)
+# print(num_zeros)
+# print(p)
+# print(np.split(zero_one_elements, 2))
+#
+# aux1, aux2 = np.split(zero_one_elements, 2)
+# # print(aux1)
+# # print(aux2)
+#
+# mA[upper_triangle] = aux1
+# mA[lower_triangle] = aux2
+#
